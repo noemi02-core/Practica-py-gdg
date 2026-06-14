e8a7aee3991b2be541382223a5e07b3ccca56c2a
Noemi Quezada <noemi@noemiquez.02@gmail.com>
2026-06-13 18:32:27 -0600

    Actualizando cambios

--- Any.py
+++ Any.py
@@ -1,3 +1,4 @@
 # Esta es una hoja para hacer pruebas y borrar las en cualquier momento
-# No es parte del proyecto, solo es para hacer pruebas
 
+# # Hacemos cosas random para probar cosas
+print("Hola mundo B) un clasico")
